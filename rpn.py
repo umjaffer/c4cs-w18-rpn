78965a4a5e569fb12ff752eb518066ff7733896d
umjaffer <ujaffer@umich.edu>
2018-02-16 11:30:41 -0500

    passed first test

--- rpn.py
+++ rpn.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
 def calculate(arg):
-	pass
+	stack = list()
+	for token in arg.split():
+		try:
+			value = int(token)
+			stack.append(value)
+		except ValueError:
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			return arg1 + arg2
 
 def main():
 	while True:
-		calculate(input('rpm calc> '))
+		print(calculate(input('rpm calc> ')))
 
 if __name__ == '__main__':
 	main()
