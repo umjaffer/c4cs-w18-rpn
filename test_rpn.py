16f593051769e0f4229c48044f88beba0a58f23b
umjaffer <ujaffer@umich.edu>
2018-02-21 22:24:19 -0500

    add test case for exponent

--- test_rpn.py
+++ test_rpn.py
@@ -15,3 +15,7 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
+    def test_exponent(self):
+    	result = rpn.calculate("2 2 ^")
+    	self.assertEqual(4, result)
+    	
